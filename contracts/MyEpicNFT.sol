eca84766b8ee1eaaeddc9541cbc63b61e291bc0f
ghidyon <gideonakunana@gmail.com>
2022-01-05 08:44:36 +0100

    read nft contract

---
+++ contracts/MyEpicNFT.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+import "hardhat/console.sol";
+
+// import some OpenZeppelin Contracts
+
+contract MyEpicNFT {
+    constructor() {
+        console.log("This is my NFT Contract. Whoa!");
+    }
+}
